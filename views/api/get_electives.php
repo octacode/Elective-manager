8a699a8c92e859767ec52822965ac4f8287e5c0b
octacode <allblue.shasha@gmail.com>
2018-12-09 14:55:40 +0530

    Specified the api requirements

--- views/api/get_electives.php
+++ views/api/get_electives.php
@@ -1,4 +1,8 @@
 <?php
     include_once('dbconnect.php');
-    echo Database::Allotmentresult();
+    // echo Database::studentregister(15554,"Kumar Shashwat","password","Sanjeev Kumar Pandey",45682,"1998-03-15","CSE","7018296471","allblue.shasha@gmail.com",1);
+
+    // echo Database::studentLogin(15554, "password");
+
+    // echo Database::publishedelectivesdetails();
 ?>
